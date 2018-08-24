989917fb0a1fcbd20b9b6a195c2964e5fbd4ea53
Varsha Mittal <varsha.mittal@ganitsoft.com>
2018-08-24 12:13:27 +0530

    employee dashboard updated

--- resources/views/la/employees/index.blade.php
+++ resources/views/la/employees/index.blade.php
@@ -34,7 +34,7 @@
                     <th>{{ $module->fields[$col]['label'] or ucfirst($col) }}</th>
                     @endforeach
                     @if($show_actions)
-                    <th>Actions</th>
+                    <th style="width: 52px;">Actions</th>
                     @endif
                 </tr>
             </thead>
