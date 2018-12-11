6a19674f582ea7ccd4d2f222d5c696574325798e
Varsha <varsha.mittal@ganitsoftech.com>
2018-12-11 19:15:42 +0530

    Leave comp off half day error resolved

--- resources/views/la/leavemaster/create.blade.php
+++ resources/views/la/leavemaster/create.blade.php
@@ -92,8 +92,8 @@ Apply For Leave
                             <input type="text" value="{{ old('LeaveReason')}}" class="form-control" name="LeaveReason" autocomplete="off" required placeholder="Reason" maxlength="180"  >   
                         </div>
                         <div class="form-group col-md-3" style="display: none" id="halfday">
-                            <label for="half_day">Half Day</label>
-                            <input class="duration form-control" type="checkbox" name="half_day" id="half_day" /> 
+                            <label for="half_day">Half Day</label><br/>
+                            <input class="duration" type="checkbox" name="half_day" id="half_day" /> 
                         </div>
                         
                         <div class="col-md-3" style="margin-top: 25px;">
